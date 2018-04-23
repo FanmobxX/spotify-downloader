5ffeb4e12efcda860fab3528fc20989c6135455d
Shane Vitarana <shanev@gmail.com>
2018-04-23 02:02:18 -0400

    using hashes instead of uuid for song names

--- fanmob-playlist.sh
+++ fanmob-playlist.sh
@@ -1,30 +1,35 @@
 #!/bin/sh
 
-python3 spotdl.py --playlist https://open.spotify.com/user/br0wnpunk/playlist/0vxbG1QMVfGWOLzbPz5OV4\?si\=OEE30SE7RhipKKRgrERBVg
-python3 spotdl.py --list=fanmob-demo.txt
+# python3 spotdl.py --playlist https://open.spotify.com/user/br0wnpunk/playlist/0vxbG1QMVfGWOLzbPz5OV4\?si\=OEE30SE7RhipKKRgrERBVg
+# python3 spotdl.py --list=fanmob-demo.txt
 
 cd "Music/";
 
+BUCKET="fanmob-media";
+mkdir "${BUCKET}/";
+
+echo [ > playlist.json
+
 for i in *.mp3;
-  do name=`echo $(uuidgen)`;
-  image="${name}.jpg";
-  video="${name}.mp4";
+  do name=`echo $i | md5`;
+  image="$BUCKET/${name}.jpg";
+  video="$BUCKET/${name}.mp4";
   ffmpeg -i "$i" -an -vcodec copy "${image}";
   ffmpeg -loop 1 -i "${image}" -i "$i" -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest "${video}";
 
   title=$(id3info "${i}" | sed -n 's/^=== TIT2 \(.*\): //p');
   artist=$(id3info "${i}" | sed -n 's/^=== TPE1 \(.*\): //p');
   cat >>playlist.json <<EOL
-  {
-    id: '${name}',
-    title: '${title}',
-    artist: '${artist}',
-    thumbnail: 'https://s3.amazonaws.com/fanmob-media/${image}',
-    source: 'https://s3.amazonaws.com/fanmob-media/${video}',
-  },
+    {
+      id: '${name}',
+      title: '${title}',
+      artist: '${artist}',
+      thumbnail: 'https://s3.amazonaws.com/fanmob-media/${image}',
+      source: 'https://s3.amazonaws.com/fanmob-media/${video}',
+    },
 EOL
 done
 
-rm *.mp3;
+echo ] >> playlist.json
 
-aws s3 sync . s3://fanmob-media --acl public-read
+aws s3 sync "$BUCKET" s3://fanmob-media --acl public-read
