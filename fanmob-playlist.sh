ab752e0a94a23da90598e4f2df816304c322ecfc
Shane Vitarana <shanev@gmail.com>
2018-04-22 20:34:25 -0400

    creating playlist

--- fanmob-playlist.sh
+++ fanmob-playlist.sh
@@ -1,16 +1,28 @@
 #!/bin/sh
 
 python3 spotdl.py --playlist https://open.spotify.com/user/br0wnpunk/playlist/0vxbG1QMVfGWOLzbPz5OV4\?si\=OEE30SE7RhipKKRgrERBVg
-
 python3 spotdl.py --list=fanmob-demo.txt
 
 cd "Music/";
 
 for i in *.mp3;
-  do name=`echo "${i%.*}"`;
-  echo $name;
-  ffmpeg -i "$i" -an -vcodec copy "${name}.jpg";
-  ffmpeg -loop 1 -i "${name}.jpg" -i "$i" -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest "${name}.mp4";
+  do name=`echo $(uuidgen)`;
+  image="${name}.jpg";
+  video="${name}.mp4";
+  ffmpeg -i "$i" -an -vcodec copy "${image}";
+  ffmpeg -loop 1 -i "${image}" -i "$i" -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest "${video}";
+
+  title=$(id3info "${i}" | sed -n 's/^=== TIT2 \(.*\): //p');
+  artist=$(id3info "${i}" | sed -n 's/^=== TPE1 \(.*\): //p');
+  cat >>playlist.json <<EOL
+  {
+    id: '${name}',
+    title: '${title}',
+    artist: '${artist}',
+    thumbnail: '${image}',
+    source: '${video}',
+  },
+EOL
 done
 
 rm *.mp3;
